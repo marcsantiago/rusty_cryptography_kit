eae40ec733c317cac6f2b59940f857adf85a56e3
Marc Santiago <marcanthonysanti@gmail.com>
2024-03-28 14:09:07 -0400

    Better Trie Implementation with Iterator

--- src/detection/trie.rs
+++ src/detection/trie.rs
@@ -5,44 +5,62 @@ use flate2::read::GzDecoder;
 use flate2::write::GzEncoder;
 use serde::{Deserialize, Serialize};
 
-// https://dev.to/timclicks/two-trie-implementations-in-rust-ones-super-fast-2f3m
-#[derive(Default, Debug, Serialize, Deserialize)]
-struct TrieNode {
-    is_end_of_word: bool,
-    #[serde(skip_serializing_if = "HashMap::is_empty")]
-    #[serde(default)]
-    children: HashMap<u8, TrieNode>,
-}
-
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct Trie {
     root: TrieNode,
 }
 
+#[derive(Default, Debug, Serialize, Deserialize, Clone)]
+struct TrieNode {
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    #[serde(default)]
+    children: HashMap<char, TrieNode>,
+    is_end_of_word: bool,
+}
+
 impl Trie {
     pub fn new() -> Self {
-        Trie {
-            root: TrieNode::default(),
-        }
+        Trie { root: TrieNode::default() }
     }
 
     pub fn insert(&mut self, word: &str) {
-        let mut current_node = &mut self.root;
-        for c in word.to_ascii_lowercase().bytes() {
-            current_node = current_node.children.entry(c).or_default();
+        let mut current = &mut self.root;
+        for ch in word.to_ascii_lowercase().chars() {
+            current = current.children.entry(ch).or_insert(TrieNode::default());
         }
-        current_node.is_end_of_word = true;
+        current.is_end_of_word = true;
     }
 
     pub fn contains(&self, word: &str) -> bool {
-        let mut current_node = &self.root;
-        for c in word.to_ascii_lowercase().bytes() {
-            match current_node.children.get(&c) {
-                Some(node) => current_node = node,
-                None => return false,
+        let mut current = &self.root;
+        for ch in word.to_ascii_lowercase().chars() {
+            if let Some(node) = current.children.get(&ch) {
+                current = node;
+            } else {
+                return false;
             }
         }
-        current_node.is_end_of_word
+        current.is_end_of_word
+    }
+
+    fn display_words(&self) {
+        let mut word = String::new();
+        self.display_words_recursive(&self.root, &mut word);
+    }
+
+    fn display_words_recursive(&self, node: &TrieNode, prefix: &mut String) {
+        for (ch, child) in &node.children {
+            prefix.push(*ch);
+            if child.is_end_of_word {
+                println!("{}", prefix);
+            }
+            self.display_words_recursive(child, prefix);
+            prefix.pop();
+        }
+    }
+
+    pub fn iter_words(&self) -> TrieIterator {
+        TrieIterator::new(&self.root)
     }
 
 
@@ -71,6 +89,39 @@ impl Trie {
     }
 }
 
+pub struct TrieIterator {
+    stack: Vec<(String, TrieNode)>,
+}
+
+impl TrieIterator {
+    fn new(node: &TrieNode) -> Self {
+        let mut stack = Vec::new();
+        stack.push((String::new(), node.clone()));
+        TrieIterator { stack }
+    }
+}
+
+impl Iterator for TrieIterator {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((mut prefix, mut node)) = self.stack.pop() {
+            if node.is_end_of_word {
+                let word = prefix.clone();
+                prefix.pop();
+                return Some(word);
+            }
+
+            for (ch, child) in node.children.iter_mut() {
+                let mut new_prefix = prefix.clone();
+                new_prefix.push(*ch);
+                self.stack.push((new_prefix, child.clone()));
+            }
+        }
+        None
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -84,12 +135,29 @@ mod test {
         trie.insert("hey");
         trie.insert("world");
 
-
         assert_eq!(trie.contains("hello"), true);
         assert_eq!(trie.contains("Hello"), true);
         assert_eq!(trie.contains("hello world"), false);
 
         trie.insert("hello world");
         assert_eq!(trie.contains("hello world"), true);
+    }
+
+    #[test]
+    fn test_display() {
+        let mut trie = Trie::new();
+        trie.insert("hello");
+        trie.insert("hi");
+        trie.insert("hey");
+        trie.insert("world");
+
+        let mut words_got: Vec<String> = Vec::new();
+        for word in trie.iter_words() {
+            words_got.push(word);
+        }
+        words_got.sort();
+        assert_eq!(vec![
+            "hello".to_string(), "hey".to_string(),
+            "hi".to_string(), "world".to_string()], words_got);
     }
 }
